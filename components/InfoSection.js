080d6cb9ef3bb62ad0b13a56f96de3152f9c8042
jabusir <jabusir@gmail.com>
2018-11-12 18:52:59 -0800

    Create InfoSection component

---
+++ components/InfoSection.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const InfoSection = () => (
+    <div className="info-section">
+        <h1 className="info-section__main-header">
+            Get an Experience Catered To You
+        </h1>
+        <div className="info-block">
+            <div className="info-content">
+                <div className="item">
+                    <div className="item-content">
+                        <div className="item__image"></div>
+                        <h3>Meet Locals</h3>
+                    </div>
+                    <div className="item__text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.
+                    </div>
+                </div>
+                <div className="item">
+                    <div className="item-content">
+                        <div className="item2__image"></div>
+                        <h3>Experience Nature</h3>
+                    </div>
+                    <div className="item__text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.
+                    </div>
+                </div>
+                <div className="item">
+                    <div className="item-content">
+                        <div className="item3__image"></div>
+                        <h3>See Sights</h3>
+                    </div>
+                    <div className="item__text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default InfoSection;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Layout from '../components/Layout';
+import InfoSection from '../components/InfoSection';
 
 export default class extends Component {
   render() {
@@ -8,6 +9,7 @@ export default class extends Component {
         <Layout>
           <main>
             <h1>Unmapped. </h1>
+            <InfoSection />
           </main>
         </Layout>
       </div>
