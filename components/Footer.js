913369813267236b76313826c540bf79252a4a78
Aaron Dean <arasdean@Aarons-MacBook-Pro.local>
2018-11-14 23:59:05 -0800

    Provides skeleton for footer

---
+++ components/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react'; 
+
+const Footer = () => (
+    <div class="footer">
+    <div class="footer-2-container">
+      <div class="row-9 w-row">
+        <div class="footer-2-column w-col w-col-4 w-col-small-small-stack w-col-medium-4"><img src="/static/Unmapped-1.png" width="100" alt="" class="image-12" />
+          <div class="social-link-wrap"><img src="/static/facebook_1facebook.png" alt="" class="image-2" /><a target="_blank" href="#" class="footer-link-2">Facebook</a></div>
+          <div class="social-link-wrap negative-ad"><img src="/static/instagram_1instagram.png" alt="" class="image-2" /><a href="#" class="footer-link-2">Instagram</a></div>
+        </div>
+        <div class="footer-2-column w-col w-col-2 w-col-small-small-stack w-col-medium-2">
+          <div class="footer-2-header">Product</div><a href="#" class="footer-2-link">Home</a><a href="#" class="footer-2-link">Search</a><a href="#" class="footer-2-link">Help Centre</a></div>
+        <div class="footer-2-column w-col w-col-2 w-col-small-small-stack w-col-medium-2">
+          <div class="footer-2-header">Product</div><a href="#" class="footer-2-link">Option</a><a href="#" class="footer-2-link">Option</a><a href="#" class="footer-2-link">Option</a><a href="#" class="footer-2-link">Option</a></div>
+        <div class="footer-2-column w-col w-col-2 w-col-small-small-stack w-col-medium-2">
+          <div class="footer-2-header">Account</div><a href="#" class="footer-2-link">Profile</a><a href="#" class="footer-2-link">Option</a><a href="#" class="footer-2-link">Messages</a></div>
+        <div class="footer-2-column w-col w-col-2 w-col-small-small-stack w-col-medium-2">
+          <div class="footer-2-header">Legal</div><a href="#" class="footer-2-link">Terms</a><a href="#" class="footer-2-link">Privacy</a><a href="#" class="footer-2-link">General FAQ</a></div>
+      </div>
+    </div>
+  </div>
+); 
+
+export default Footer; 
