9f2a07cebd6b8f180d4c80d6685450d72e395684
jabusir <jabusir@gmail.com>
2018-11-15 14:52:05 -0800

    Fix className warning in HeroSection and maxLength warning in SearchBox

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -4,11 +4,11 @@ import SearchBox from './SearchBox';
 import '../styles/_hero.scss';
 
 const HeroSection = (props) => (
-    <div class="hero_section">
+    <div className="hero_section">
         <NavBar /> 
-        <div class="hero_container">
-            <div class="hero_box">
-                <h1 class="heading">A friend in every city.</h1>
+        <div className="hero_container">
+            <div className="hero_box">
+                <h1 className="heading">A friend in every city.</h1>
                 <SearchBox />
             </div>
         </div>
