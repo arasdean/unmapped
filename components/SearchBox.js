9169e4b3a99bf93b542c111ef672e03d8ac24d47
Aaron Dean <arasdean@Aarons-MacBook-Pro.local>
2018-11-14 23:13:46 -0800

    Fixes hero section

--- components/SearchBox.js
+++ components/SearchBox.js
@@ -3,7 +3,7 @@ import '../styles/main.scss'
 const SearchBox = () => (
     <div class="searchbox">
         <div class="form-block w-form">
-            <form id="email-form" name="email-form" data-name="Email Form"><input type="text" class="search_input" maxlength="256" name="search_input" data-name="search_input" placeholder="Best restaurants, popular bars..." id="search_input" /></form>
+            <form id="email-form" name="email-form" data-name="Email Form"><input type="text" class="search_input w-input" maxlength="256" name="search_input" data-name="search_input" id="search_input" /></form>
         </div>
         <a href="interests.html" class="button w-button">Meet Locals</a>
     </div>
