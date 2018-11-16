caf40fb52a77d4130d9fd4d24be087efdf13c319
jabusir <jabusir@gmail.com>
2018-11-15 21:32:09 -0800

    Finalize footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,15 +4,11 @@ const Footer = () => (
     <div class="footer">
       <div class="footer-2-container">
           <div class="row-9 w-row">
-              <a href="#" class="footer-2-link">Home</a>
-              <a href="#" class="footer-2-link">Search</a>
-              <a href="#" class="footer-2-link">Help Centre</a>
-              <a href="#" class="footer-2-link">Profile</a>
-              <a href="#" class="footer-2-link">Option</a>
-              <a href="#" class="footer-2-link">Messages</a>
-              <a href="#" class="footer-2-link">Terms</a>
-              <a href="#" class="footer-2-link">Privacy</a>
-              <a href="#" class="footer-2-link">General FAQ</a>
+              <a href="/about" class="footer-2-link">About</a>
+              <a href="/contact-us" class="footer-2-link">Contact Us</a>
+              <a href="/careers" class="footer-2-link">Careers</a>
+              <a href="/faq" class="footer-2-link">FAQ</a>
+              <a href="/request-city" class="footer-2-link">Request City</a>
           </div>
       </div>
       <div class="social-link-wrap"><img src="/static/facebook_1facebook.png" alt="" class="image-2" /><a target="_blank" href="#" class="footer-link-2"></a></div>
