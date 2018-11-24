a23b34bedb869e00899d80f1346a62434e89c5dd
Aaron Dean <arasdean@Aarons-MacBook-Pro.local>
2018-11-24 09:11:25 -0800

    additional components

--- components/TravelSummary.js
+++ components/TravelSummary.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const TravelSummary = (props) => (
+<<<<<<< HEAD
     <div className="summary">
         <div className="summary-header">TRIP SUMMARY</div>
         <div className="summary-section">
@@ -27,6 +28,25 @@ const TravelSummary = (props) => (
         <div className="pricing-container summary-section">
             <div className="summary-total">TOTAL</div>
             <div className="summary-total">${props.data.total}</div>
+=======
+    <div>
+        <div className="summary-text">Number of tourists</div>
+        <div className="summary-text">{props.data.tourists}</div>
+        <div className="summary-text">Origin of the trip</div>
+        <div className="summary-text">{props.data.destination}</div>
+        <div className="summary-text">Dates of the trip</div>
+        <div className="summary-text">{props.data.dates}</div>
+        <div className="summary-text">List of hobbies</div>
+        <div className="summary-text">{props.data.hobbies}</div>
+        <div className="gray-line"></div>
+        <div className="pricing-container">
+            <div className="summary-text">Fees & taxes</div>
+            <div className="summary-text">$10</div>
+        </div>
+        <div className="pricing-container">
+            <div className="summary-total"></div>
+            <div className="summary-total">{props.data.total}</div>
+>>>>>>> df260c9... Create summary page, and corresponding component
         </div>
     </div>
 );
--- pages/summary.js
+++ pages/summary.js
@@ -1,10 +1,17 @@
 import TravelSummary from "../components/TravelSummary";
+<<<<<<< HEAD
 import Layout from "../components/Layout";
+=======
+>>>>>>> df260c9... Create summary page, and corresponding component
 
 export default class summary extends React.Component {
     state = {
         tourists: 3,
+<<<<<<< HEAD
         destination: 'San Francisco, CA',
+=======
+        location: 'San Francisco, CA',
+>>>>>>> df260c9... Create summary page, and corresponding component
         dates: 'December 11th, 2018 - December 14th, 2018',
         hobbies: ['basketball', 'bridges', 'waterparks'],
         total: 150,
@@ -16,6 +23,7 @@ export default class summary extends React.Component {
 
     render() {
         return (
+<<<<<<< HEAD
             <Layout>
                 <h1 className="header">Personal Information</h1>
                 <div className="dashboard">
@@ -33,6 +41,22 @@ export default class summary extends React.Component {
                 </div>
             </Layout>
 
+=======
+            <div className="dashboard">
+                <div className="col personal-info">
+                    <h1 className="col-header">Personal Information</h1>
+                    <div className="email-form">
+                        <input id="contactMail" className="contact-form" type="email" placeholder="Email Address" />
+                        <input id="confirmMail" className="contact-form" type="email" placeholder="Confirm Email Address" /> 
+                    </div>
+                    <input id="contactNumber" className="contact-form" type="text" placeholder="Phone Number" />
+                </div>
+                <div className="col summary">
+                    <h1 className="col-header">Trip Summary</h1>
+                    <TravelSummary data={Object.assign({}, this.state)} />
+                </div>
+            </div>
+>>>>>>> df260c9... Create summary page, and corresponding component
         );
     }
 }
