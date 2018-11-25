be8c7b5cfa5a180f200b0f501e300d7cdfdde4d4
Aaron Dean <arasdean@Aarons-MacBook-Pro.local>
2018-11-24 21:26:00 -0800

    Adds classnames for NavBar file to resolve error message

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,23 +1,23 @@
 import '../styles/main.scss'
 
 const NavBar = () => (
-    <div class="nav_section">
-        <div class="nav_row">
-            <div class="column logo"> <img src="/static/Unmapped-1.png" width="206" alt="" /> </div>
-            <div class="column">  <a href="#" class="nav_link w-inline-block">
+    <div className="nav_section">
+        <div className="nav_row">
+            <div className="column logo"> <img src="/static/Unmapped-1.png" width="206" alt="" /> </div>
+            <div className="column">  <a href="#" className="nav_link w-inline-block">
                 <h4>How it works</h4>
             </a>
-                <a href="#" class="nav_link w-inline-block">
+                <a href="#" className="nav_link w-inline-block">
                     <h4>What&#x27;s included</h4>
                 </a>
             </div>
-            <div class="column"> <a href="#" class="nav_link w-inline-block">
+            <div className="column"> <a href="#" className="nav_link w-inline-block">
                 <h4>FAQs</h4>
             </a>
-                <a href="#" class="nav_link w-inline-block">
+                <a href="#" className="nav_link w-inline-block">
                     <h4>Contact</h4>
                 </a>
-                <a href="#" class="nav_link w-inline-block">
+                <a href="#" className="nav_link w-inline-block">
                     <h4>My Tours</h4>
                 </a> </div>
         </div>
