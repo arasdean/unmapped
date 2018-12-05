8466d9f5215970d713f3adb74a3c4bf841b4d5b0
jabusir <jabusir@gmail.com>
2018-12-04 17:06:48 -0800

    Style lading page and user details page

--- src/components/InfoSection.js
+++ src/components/InfoSection.js
@@ -7,7 +7,7 @@ const InfoSection = () => (
         </h1>
         <div className="info-block">
             <div className="info-content">
-                <div className="item">
+                <div className="item-home">
                     <div className="item-content">
                         <div className="item__image"></div>
                         <h3>Meet Locals</h3>
@@ -16,7 +16,7 @@ const InfoSection = () => (
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.
                     </div>
                 </div>
-                <div className="item">
+                <div className="item-home">
                     <div className="item-content">
                         <div className="item2__image"></div>
                         <h3>Experience Nature</h3>
@@ -25,7 +25,7 @@ const InfoSection = () => (
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.
                     </div>
                 </div>
-                <div className="item">
+                <div className="item-home">
                     <div className="item-content">
                         <div className="item3__image"></div>
                         <h3>See Sights</h3>
