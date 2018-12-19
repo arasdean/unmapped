b40a96adede56fdf49f1057ad5a8e428eb9bc6fc
jabusir <jabusir@gmail.com>
2018-12-19 14:04:42 -0800

    Restructure guide Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,10 +7,16 @@ export default function Card(props) {
     return (
         <div class="_6-col">
             <div class="team3-profile-card">
-                <img className="team3-avatar" src={url} />
-                <h2 class="team3-name">{name}</h2>
-                <div class="tagline team3-position">{location}</div>
-                <p>{bio}</p>
+                <div className="team3-content-container">
+                    <div className="team3-avatar-container">
+                        <img className="team3-avatar" src={url} />
+                    </div>
+                    <div className="team3-content">
+                        <h2 class="team3-name">{name}</h2>
+                        <div class="tagline team3-position">{location}</div>
+                        <p>{bio}</p>
+                    </div>
+                </div>
                 <div class="team3-buttons-wrap"><a href="#" class="btn team3-left-button w-button" onClick={(e) => props.save(e, name)}>Connect</a></div>
             </div>
         </div>
--- src/components/Guides.jsx
+++ src/components/Guides.jsx
@@ -10,14 +10,14 @@ const data = [
       "url": "https://i.imgur.com/fgxxyew.jpg",
       "languages": "English, Rudimentary French",
       "gender": "Male",
-      "bio": "Paul, a San Francisco native, professional actor, and world traveler, who has spent time in many countries around the world. Having worked for years in the local tech industry Paul shifted his career to leverage his strengths, and began using his performance and research skills to create comprehensive tours of the San Francisco Bay Area, featuring a mix of attractions, ranging from the “Must-see” (Golden Gate Bridge, Chinatown, etc) to the obscure well-kept secret locales: best local dives, best used bookstore, best dim sum in SF (hint: it’s *not* in Chinatown). When not in SF, Paul can be found working in Shakespeare theaters around the world (Shakespeare’s Globe in London, The Pop-up Globe in Auckland New Zealand, and, recently, the streets of Seoul, Korea)!"
+      "bio": "Paul, in addition to being a highly sought-after tour guide, is a San Francisco native, a professional actor, and world traveler, who has spent time in many countries around the world.  H"
     },
     {
       "name": "Obaida Albaroudi",
       "languages": "Arabic, English, Basic Spanish",
       "url": "https://i.imgur.com/Y7f7CMu.jpg",
       "gender": "Male",
-      "bio": "Obaida is an avid basketball fan as well as a big time foodie. He not only has a list of the top restaurants in SF/Bay Area but also across the world. He is in touch with the corporate world in SF and can help plan visits across the different companies in the area while also talking to the culture of SF. If you are looking to better understand the food scene and the overall culture of SF and the bay area he is the go to person."
+      "bio": "Obaida is an avid basketball fan as well as a big time foodie. He not only has a list of the top restaurants in SF/Bay Area but also across the world."
     },
     {
       "name": "Evonnie Lovely Contreras",
@@ -31,7 +31,7 @@ const data = [
       "languages": "English",
       "url": "https://images.unsplash.com/photo-1542838686-b08706f6f2d1?ixlib=rb-0.3.5&s=34973521d142d68c7c10b8c4a5c0ec4e&auto=format&fit=crop&w=668&q=80",
       "gender": "Male",
-      "bio": "Jesse Parker is a Gay 26-year-old Bay Area native. He studies art history and visual culture in Berkeley and was an art framer for a time. As a result, he has become a purveyor of the art scenes in San Francisco and greater bay area. Although he lives in Oakland he loves to get lost in San Francisco whenever the opportunity arises finding new restaurants, cafés, parks to hike and places to hang out. When the mood strikes he has been known to take in the vibrant and varied night life that San Francisco has to offer. When time permits he loves to cook and host dinner parties for his friends."
+      "bio": "Jesse Parker is a Gay 26-year-old Bay Area native. He studies art history and visual culture in Berkeley and was an art framer for a time. As a result, he has become a purveyor of the art scenes in San Francisco and greater bay area. Although he lives in Oakland he loves to get lost in San Francisco whenever the opportunity arises finding new restaurants, cafés, parks to hike and places to hang out."
     },
     {
       "name": "Hossain",
@@ -45,7 +45,7 @@ const data = [
       "languages": "English, Hindi",
       "url": "https://i.imgur.com/DMgCGoI.jpg",
       "gender": "Male",
-      "bio": "Raj is a lover of the natural life, and the bay area is an indulgent place. He is an avid hiker and actively seeks out unique places and stunning natural vistas in SF, East Bay (Berkeley, Oakland) and the North Bay counties. He spends a lot of his recreational hours with friends visiting the many local breweries, wineries and artisinal farms (produce, cheeses etc.) that call this region home. When not eating, drinking and walking/hiking his way around town, Raj geeks out on the contemporary history and culture of San Francisco - from the 'Summer of Love' to the Beat Poets to the Black Panthers to Alice Water's local food movement."
+      "bio": "Raj is a lover of the natural life, and the bay area is an indulgent place. He is an avid hiker and actively seeks out unique places and stunning natural vistas in SF, East Bay (Berkeley, Oakland) and the North Bay counties. He spends a lot of his recreational hours with friends visiting the many local breweries, wineries and artisinal farms."
     }
   ]
 
