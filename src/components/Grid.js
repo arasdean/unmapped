7cd7d7237bf1336f99d0e7fd97e87ad0fb884fdd
Annisa Hadi <annisa.hadi@gmail.com>
2018-12-22 16:30:59 -0800

    add grid layout for mobile

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -7,24 +7,44 @@ import Item from "./Item";
 
 
 export default class Grid extends React.Component {
+  state = {
+    isMobile: null
+  }
+
+  componentDidMount() {
+    this.setState({
+            isMobile: window.innerWidth < 435
+        });
+  }
+
+
   render() {
     const { clickedState } = this.props;
+    const { isMobile } = this.state;
+    console.log("mobile", this.state.isMobile);
     return (
-      <div className='grid'>
-        {this.props.data.map(row => (
-          <div className="row">
-            {[0, 1, 2].map((num) => <Item
-              key={row[num].key}
-              header={row[num].name}
-              handle={this.props.handler}
-              clicked={clickedState[row[num].name]}
-            />
-           )}
-          </div>
-        ))}
-        <div className='row' style={{display: 'flex', justifyContent: 'center'}}> 
-           <Button onClick={this.props.save}> Next </Button>
-         </div>
+      <div>
+
+        <div className={isMobile ? 'grid-mobile-wrapper' : 'grid-wrapper'}>
+          <div className={isMobile ? 'grid-mobile' : 'grid'}>
+            {this.props.data.map(row => (
+              <div className={isMobile ? 'grid-row' : 'row'}>
+                {[0, 1, 2].map((num) => <Item
+                  key={row[num].key}
+                  header={row[num].name}
+                  handle={this.props.handler}
+                  clicked={clickedState[row[num].name]}
+                />
+               )}
+              </div>
+            ))}
+            </div>
+        </div>
+
+            <div className='row' style={{display: 'flex', justifyContent: 'center'}}> 
+               <Button onClick={this.props.save}> Next </Button>
+             </div>
+
       </div>
     );
   }
