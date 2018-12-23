96f7ebbe39c2fc2d97975f7ec1b5e2935e0d7c39
Annisa Hadi <annisa.hadi@gmail.com>
2018-12-22 21:52:52 -0800

    fix interests grid mobile ui

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -7,44 +7,39 @@ import Item from "./Item";
 
 
 export default class Grid extends React.Component {
-  state = {
-    isMobile: null
-  }
-
-  componentDidMount() {
-    this.setState({
-            isMobile: window.innerWidth < 435
-        });
-  }
-
-
+  
   render() {
     const { clickedState } = this.props;
-    const { isMobile } = this.state;
-    console.log("mobile", this.state.isMobile);
     return (
       <div>
 
-        <div className={isMobile ? 'grid-mobile-wrapper' : 'grid-wrapper'}>
-          <div className={isMobile ? 'grid-mobile' : 'grid'}>
+        <div className='grid-wrapper'>
+          <div className='grid'>
             {this.props.data.map(row => (
-              <div className={isMobile ? 'grid-row' : 'row'}>
-                {[0, 1, 2].map((num) => <Item
+                [0, 1, 2].map((num) => (
+                <div className='row2'>
+                <Item
                   key={row[num].key}
                   header={row[num].name}
                   handle={this.props.handler}
                   clicked={clickedState[row[num].name]}
                 />
-               )}
-              </div>
+                </div>
+               ))
             ))}
-            </div>
-        </div>
 
+            <br />
+            <br />
+            
             <div className='row' style={{display: 'flex', justifyContent: 'center'}}> 
                <Button onClick={this.props.save}> Next </Button>
              </div>
 
+            <br />
+            <br />
+
+            </div>
+        </div>
       </div>
     );
   }
