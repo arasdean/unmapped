8b70087ce3f1fb2b090b98ef6cac7188a2331e27
Annisa Hadi <annisa.hadi@gmail.com>
2018-12-23 20:27:51 -0800

    add bootstrap and background images to interests grid

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -11,36 +11,30 @@ export default class Grid extends React.Component {
   render() {
     const { clickedState } = this.props;
     return (
-      <div>
-
-        <div className='grid-wrapper'>
-          <div className='grid'>
+        <div className='container'>
             {this.props.data.map(row => (
-                [0, 1, 2].map((num) => (
-                <div className='row2'>
-                <Item
+              <div className='row'>
+                {[0, 1, 2].map((num) => (<Item
+                  image={row[num].image}
                   key={row[num].key}
                   header={row[num].name}
                   handle={this.props.handler}
                   clicked={clickedState[row[num].name]}
                 />
-                </div>
-               ))
+               ))}
+               </div>
             ))}
 
             <br />
             <br />
-            
+
             <div className='row' style={{display: 'flex', justifyContent: 'center'}}> 
                <Button onClick={this.props.save}> Next </Button>
              </div>
 
             <br />
             <br />
-
-            </div>
         </div>
-      </div>
     );
   }
 }
