446c6e07df8e502460d886b5a0e8e96d46762ad5
Aaron Dean <arasdean@Aarons-MacBook-Pro.local>
2019-01-01 14:41:49 -0800

    Fixes remaining UI issues

--- src/pages/Summary.js
+++ src/pages/Summary.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Radio, Input} from 'antd';
+import { Radio, Input } from 'antd';
 import TravelSummary from "../components/TravelSummary";
 import Layout from "../components/Layout";
 
-const RadioGroup = Radio.Group; 
+const RadioGroup = Radio.Group;
 const { TextArea } = Input;
 
 export default class Summary extends React.Component {
@@ -19,10 +19,10 @@ export default class Summary extends React.Component {
                             <div className="contact-header">About You</div>
                             <div className="email-form">
                                 <input id="email" onChange={this.props.handleChange('email')} className="contact-input" type="email" placeholder="Email Address" />
-                                <RadioGroup value={values.values.contactPref} onChange={handleChange('contactPref')} style={{width: '50%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-                                    Preferred Contact: <br /> 
-                                    {['Whatsapp', 'Email'].map((type) => <Radio value={type}> {type} </Radio> )} 
-                                </RadioGroup> 
+                                <RadioGroup value={values.values.contactPref} onChange={handleChange('contactPref')} style={{ width: '50%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                    Preferred Contact: <br />
+                                    {['Whatsapp', 'Email'].map((type) => <Radio value={type}> {type} </Radio>)}
+                                </RadioGroup>
                             </div>
                             <div className='email-form'>
                                 <input id="name" onChange={this.props.handleChange('name')} className="contact-input" type="name" placeholder="Full name" />
@@ -31,15 +31,18 @@ export default class Summary extends React.Component {
                             <div className='email-form'>
                                 <input id="name" onChange={this.props.handleChange('name')} className="contact-input" type="name" placeholder="Referral Code (optional)" />
                             </div>
-                             <button style={{ background: '#2cb066', marginTop: '30px' }} className="submit-button" onClick={() => submit(values)}>LET'S GO</button>
+                            
                         </div>
 
                     </div>
                     <div className="col summary">
-                        <TravelSummary data={this.props.values}/>
-                        <br /> 
-                        <TextArea onChange={this.props.handleChange('extra')}  placeholder="Is there anything else you want us to know?"/> 
+                        <TravelSummary data={this.props.values} />
+                        <br />
+                        <TextArea onChange={this.props.handleChange('extra')} placeholder="Is there anything else you want us to know?" />
                     </div>
+                </div>
+                <div className="dashboard-bottom">
+                    <button style={{ background: '#2cb066', marginTop: '30px' }} className="submit-button" onClick={() => submit(values)}>LET'S GO</button>
                 </div>
             </Layout>
         );
